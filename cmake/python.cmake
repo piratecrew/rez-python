6004e74c926f9c5eedc0f810b10ecc15141eb336
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-19 23:49:08 +0200

    Using full path to library in cmake

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,4 +1,4 @@
 find_package(Python)
-set(python_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS})
-set(python_LIBRARY_DIRS ${PYTHON_LIBRARY_DIRS})
-set(python_LIBRARIES ${PYTHON_LIBRARIES})
+set(python_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
+get_filename_component(python_LIBRARY_DIRS ${PYTHON_LIBRARY} PATH)
+set(python_LIBRARIES ${PYTHON_LIBRARY})
